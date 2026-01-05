4f0cdefcd1207fba8a4b65ea961bdb14c31ea59b
Pranav Waingade <pranavwaingade25@gmail.com>
2026-01-05 20:46:48 +0530

    Add files via upload

---
+++ Firstclass.java
@@ -0,0 +1,53 @@
+
+// import java.util.Scanner;
+class Firstclass{
+    public static void main(String[] args) {
+        //!--------------------Output------------------------!
+
+        // System.err.println("Hello World");
+        // System.out.print("Hello World with Java");
+        // System.out.println("Hello World with Java");
+
+
+
+
+        //!-------------------Variables---------------------!
+        
+        //In the variables can stored the values.
+
+        // int age=21;
+        // System.out.println(age);
+
+        // int a=10;
+        // int b=15;
+        // System.out.println(a+b);
+        // System.out.println(b-a);
+
+
+        //!-------------------Constant----------------------!
+        
+        // Here the can constant value it can't be changed constant valued in there like Pi(3.14)
+
+
+        //!------------------Data Types--------------------!
+
+        // 1) Primitive :- byte, Short ,char , boolean ,int,long ,floot,bouble.
+        // 2) Non Primpitve:- String ,Array , Class , Objects , Interfaces.
+
+
+        //!--------------------Input----------------------!
+
+        // nextLine(),nextFloot(),nextInt() :- it can print the over all line print continous.
+        
+        // Scanner sc=new Scanner(System.in);
+        // String name= sc.next();
+        // System.out.println(name);
+
+        // Scanner sc= new Scanner(System.in);
+        // int a= sc.nextInt();
+        // int b= sc.nextInt();
+        // int sum =a+b;
+        // System.out.println(sum);
+
+    }
+}
