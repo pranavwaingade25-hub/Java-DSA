0139c8786aeb96defdbbaec516a939176102d760
Pranav Waingade <pranavwaingade25@gmail.com>
2026-01-14 12:47:47 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Arrays{
+    public static void main(String[] args) {
+
+        //---------------------Defining Arryas-----------------------//
+        // //    type [] arrayName = new type[size];
+        // //    type [] arrayName = {...aray elemny....}
+
+
+        // // int []  marks = new int[3];    
+        // int marks[] = new int[3];    // both are correct 
+        // marks[0]=97;
+        // marks[1]=98;
+        // marks[2]=95;
+        // // System.out.println(marks[0]);
+        // // System.out.println(marks[1]);
+        // // System.out.println(marks[2]);
+        // for(int i=0 ; i<3 ;i++){
+        //     System.out.println(marks[i]);
+        // }
+
+
+        //--------------------------------------------------------------------------------\\
+        // int result[]= {50 ,50,50};
+        // for(int i=0 ; i<3 ;i++){
+        //     System.out.println(result[i]);
+        // }
+
+
+        //--------------------------------------------------------------------------------\\
+        // Scanner sc = new Scanner(System.in);
+        // int size =sc.nextInt();
+        // int number[] = new int[size];
+        // for(int i=0;i<size ;i++){
+        //     number[i] =sc.nextInt();
+        // }
+        // for(int i=0 ;i<size ;i++){
+        //     System.out.println(number[i]);
+        // }
+
+        //--------------------------------------------------------------------------------\\
+        // Q.Take an Array as input from the user. search for a given number x and print the index at which it occurs.
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the size of array:- ");
+        // int size =sc.nextInt();
+        // System.out.print("Enter the elements of array:- ");
+        // int number[] =  new int [size];
+        // //Input
+        // for(int i=0; i<size;i++){
+        //     number[i]=sc.nextInt();
+        // }
+        // System.out.print("Enter the valu you can search it");
+        // int search= sc.nextInt();
+        // //Output
+        // for(int i=0 ;i<number.length ;i++){
+        //     if (search == number[i]) {
+        //         System.out.println("The given numbet stored in index:- "+i);
+        //     }
+        // }
+
+
+        
+
+
+
+
+        
+    }
+}
