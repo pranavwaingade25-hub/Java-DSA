d89fe48bf954344a4b19edffc13c67d96cdaddc8
Pranav Waingade <pranavwaingade25@gmail.com>
2026-01-16 20:22:14 +0530

    Add files via upload

---
+++ Arrays_2D.java
@@ -0,0 +1,61 @@
+
+
+
+
+public class Arrays_2D {
+    public static void main(String[] args) {
+
+        // Declreation
+        // type arrayname[] = new type [row] [Columns]
+        // Scanner sc= new Scanner(System.in);
+        // int row=sc.nextInt();
+        // int Columns=sc.nextInt();
+        // int numbers[] [] =new int [row] [Columns];
+        // //Input
+        // for (int i = 0; i < row; i++) {
+        //     for (int j = 0; j < Columns; j++) {
+        //         numbers [i] [j]=sc.nextInt();
+        //     }
+        // }
+        // //Output
+        // for (int i = 0; i < row; i++) {
+        //     for (int j = 0; j < Columns; j++) {
+        //         System.err.print(numbers [i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }   
+
+
+        // Q.Take a matrix as input from the user. Search for a given number x and print the indices at which it occurs.
+        // Scanner sc =new Scanner(System.in);
+        // int rows =sc.nextInt();
+        // int Columns =sc.nextInt();
+        // int matrix[] [] =new int [rows] [Columns];
+        // //Input 
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < Columns; j++) {
+        //         matrix[i][j] =sc.nextInt();
+        //     }  
+        // }
+        // //Output
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < Columns; j++) {
+        //         System.out.print(matrix[i][j]+" ");
+        //     } 
+        //     System.err.println(); 
+        // }
+        // int search =sc.nextInt();
+        // System.out.println("Search the values index :- ");
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < Columns; j++) {
+        //         if(search == matrix[i][j]){
+        //             System.err.println("index number is:- "+i+" "+j);
+        //             break;
+        //         }
+        //     }  
+        // }
+
+
+        
+    }
+}
---
+++ Questions_Arrays.java
@@ -0,0 +1,78 @@
+
+
+public class Questions_Arrays {
+    public static void main(String[] args) {
+
+        //Q1. Take an array of names as input from the user and print them on the screen.
+
+        // Scanner sc= new Scanner(System.in);
+        // System.err.print("Enter the size of array:- ");
+        // int size =sc.nextInt();
+        // String name[] =new String[size];
+        // System.err.println("Enter the names:- ");
+        // for (int i = 0; i < name.length; i++) {
+        //     name[i]=sc.next();
+        // }
+        // System.err.println("--------------------Display the Array-------------");
+        // //Output
+        // for(int i=0 ;i< name.length;i++){
+        //     System.err.println(name[i]);
+        // }
+
+
+        // Q2. Find the maximum & minimum number in an array of integers. 
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the size of Array:- ");
+        // int size = sc.nextInt();
+        // System.err.println("Enter the elements of the array:- ");
+        // int numbers[] =new int[size];
+        // //Input
+        // for (int i = 0; i < numbers.length ; i++) {
+        //     numbers[i]= sc.nextInt();
+        // }
+        // //OutPut
+        // int maximum=0;
+        // int minimum=numbers[0];
+        // for (int i = 0; i < numbers.length; i++) {
+        //     if(numbers[i] > maximum){
+        //         maximum = numbers[i];
+        //     }
+        //     if(numbers[i] <minimum){
+        //         minimum =numbers[i];
+        //     }  
+        // }
+        // System.err.println("Largest number is: "+maximum);
+        // System.err.println("Smallest number is: "+minimum);
+
+
+        //// Q3. Take an array of numbers as input and check if it is an array sorted in ascending order.
+        ////     Eg : { 1, 2, 4, 7 } is sorted in ascending order.
+        ////     {3, 4, 6, 2} is not sorted in ascending order.
+        // Scanner sc =new Scanner(System.in);
+        // System.err.println("Enter the size arrays:- ");
+        // int size=sc.nextInt();
+        // int number[] =new int[size];
+        // System.err.println("Enter the elements of array:- ");
+        // //Input
+        // for (int i = 0; i < number.length; i++) {
+        //     number[i] =sc.nextInt();
+        // }
+        // boolean Ascending =true;
+
+        // for (int i = 0; i < number.length-1; i++) {
+        //     if(number[i] > number[i+1]){
+        //         Ascending =false;
+        //     }
+            
+        // }
+
+        // if(Ascending){
+        //     System.err.println("The Given array is Asending Array");
+        // }
+        // else{
+        //     System.err.println("The Given Array is not Asendinf Array");
+        // }
+
+    }
+    
+}
