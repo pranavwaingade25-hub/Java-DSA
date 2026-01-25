cae28262eb3dd6d6c51784e2a79c9cea052fadaa
Pranav Waingade <pranavwaingade25@gmail.com>
2026-01-25 12:34:19 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,102 @@
+public class Operators {
+     public static void main(String[] args) {
+        // Symbols that tell compiler to perfrom some operation.
+
+        // 1. Binary :-[ + , - , * , / , % ]
+        
+        // int a=10;
+        // int b= 5;
+        // System.out.println("The sum of number:- "+(a+b));
+        // System.out.println("The sub of number:- "+(a-b));
+        // System.out.println("The Product of number:- "+(a*b));
+        // System.out.println("The divide of number:- "+(a/b));
+        // System.out.println("The Module of number:- "+(a%b));  // they calculate the remiander
+
+        //==============================================================================================================================
+
+        // 2. Unary :- [ ++ , -- ];
+        // Pre Increment (++a) step:- 1)changed value.  2)used value
+
+        // int a=10;
+        // int b=0;
+        // b=++a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        //Post Increment (a++) step:- 1)Used value 2)changed value 
+
+        // int c =10;
+        // int d =0;
+        // d=c++;
+        // System.out.println(c);
+        // System.out.println(d);
+
+        //==============================================================================================================================
+
+        // 3.Relational Operators :- [ == , != , > , < , >= , <= ]
+
+        // int a =10;
+        // int b= 5;
+        // System.out.println(a== b);
+        // System.out.println(a!=b);
+        // System.out.println(a > b);
+        // System.out.println(a < b);
+        // System.out.println(a >= b);
+        // System.out.println(a <= b);
+
+        //==============================================================================================================================
+
+        // 4.Logical Operators :- [ && , || , ! ]
+        // // #1 && (and) both condtions are ture then return true
+        // int a =10;
+        // int b =20;
+        // int c= 30;
+        // System.out.println((a < b) && (a < c));
+
+        // // #2 || ( or) one condtion is true return true 
+        // System.out.println(( a < b) || (a > c));
+
+        // // #3 ! (noy) they van revese the reurn value
+        // System.out.println(!(a < b));
+
+        //==============================================================================================================================
+
+        //********************************************************* Theory part ****************************************************
+
+        // Binary number stytem (base 2)
+ 
+        //   0    = 0000    
+        //   1    = 0001
+        //   2    = 0010
+        //   3    = 0011
+        //   4    = 0100
+        //   5    = 0101
+        //   6    = 0110
+        //   7    = 0111
+        //   8    = 1000
+        //   9    = 1001
+        //   10   = 1010
+        //   11   = 1011
+        //   12   = 1100
+        //   13   = 1101
+        //   14   = 1110
+        //   15   = 1111
+
+
+        //===============================================================================================================================================================
+
+        // 5. Bitwiase  Operators :- [ & (Binary AND) , | (Binary OR) , ^ (Binary XOR) , ~ (Binary one's Complement) , << (Binary left Shift) , >> (Binary right Shift) ]
+
+       
+
+
+        //======================================================================================================================
+
+        // 6. Assignment Operators:- [ = , += , -= , *= , /= ]
+
+
+
+
+
+     }
+}
